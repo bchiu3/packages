eb2c1b8e632603ac16383e15b03c263c8af377ea
mlite <mlite@users.noreply.github.com>
2020-10-20 22:04:23 -0700

    import lws that we can bulid

---
+++ libwebsockets-2.4.2/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+mypath=$(dirname $(readlink -f $0))
+
+mkdir -p ${mypath}/build
+
+cd ${mypath}/build
+
+cmake -G"Unix Makefiles" \
+  -DLWS_WITH_ZLIB= 1 \
+  -DLWS_WITH_SSL=1 \
+  -DLWS_WITH_WOLFSSL=1 \
+  -DLWS_WITHOUT_EXTENSIONS=1 \
+  -DLWS_WITH_ZIP_FOPS=0 \
+  -DLWS_WOLFSSL_LIBRARIES=$mypath/../build/lib/libwolfssl.a \
+  -DLWS_WOLFSSL_INCLUDE_DIRS=$mypath/../build/include \
+  -DCMAKE_C_COMPILER=$CC \
+  -DCMAKE_C_FLAGS="-fno-stack-protector -fno-PIC -Wno-error" \
+  -DLWS_WITHOUT_TESTAPPS=1 \
+  ..
