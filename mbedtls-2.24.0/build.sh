7b7872a5c2776d8594e19bb4b22b926f388af179
mlite <mlite@users.noreply.github.com>
2020-11-01 05:16:03 -0800

    use absolute path

--- mbedtls-2.24.0/build.sh
+++ mbedtls-2.24.0/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-CFLAGS="-I$PWD/configs -DMBEDTLS_CONFIG_FILE='<config-mini-tls1_1.h>'" make
-DESTDIR=$PWD/../build make install
+mypath=$(dirname $(readlink -f $0))
+CFLAGS="-I${mypath}/configs -DMBEDTLS_CONFIG_FILE='<config-mini-tls1_1.h>'" make
+DESTDIR=${mypath}/../build make install
