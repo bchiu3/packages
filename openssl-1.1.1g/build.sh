c7a5c84f58dfef62d02ab51d97b90485b313a0f7
mlite <mlite@users.noreply.github.com>
2020-12-09 18:58:49 -0800

    import the version that is known to build with curl

---
+++ openssl-1.1.1g/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#./Configure no-threads no-shared no-asm 386 --prefix=$(pwd)/../build linux-generic32
+
+./config --prefix=$(pwd)/../build --debug no-asm no-pic no-shared no-threads
+make -j 8
+make install
