e338cdc8a72223120934b55501c46ec51bd034a7
mlite <mlite@users.noreply.github.com>
2020-12-10 09:58:50 -0800

    we should use shared as little as possible to avoid bugs

--- libuv-1.40.0/build.sh
+++ libuv-1.40.0/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 ./autogen.sh
 ./autogen.sh
-./configure --prefix=$(pwd)/../build
+./configure --prefix=$(pwd)/../build \
+			--enable-shared=no \
+			--enable-static=yes
+
 make -j 8
 make install
