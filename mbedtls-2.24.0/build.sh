9fe9e59f5888a3f40de13549222e52557e2c84a7
mlite <mlite@users.noreply.github.com>
2020-12-12 13:34:55 -0800

    add c11

--- mbedtls-2.24.0/build.sh
+++ mbedtls-2.24.0/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mypath=$(dirname $(readlink -f $0))
-CFLAGS="-I${mypath}/configs -DMBEDTLS_CONFIG_FILE='<config-mini-tls1_1.h>'" make
+CFLAGS="-I${mypath}/configs -DMBEDTLS_CONFIG_FILE='<config-mini-tls1_1.h>' -std=c11" make
 DESTDIR=${mypath}/../build make install
