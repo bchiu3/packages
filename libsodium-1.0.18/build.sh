4b71af9602b157ffad8787cde3e9580049810c2d
mlite <mlite@users.noreply.github.com>
2021-04-07 14:56:22 -0700

    import libsodium

---
+++ libsodium-1.0.18/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+export LDFLAGS=-L$(pwd)/../build/lib -static
+export CFLAGS=-static
+export LT_SYS_LIBRARY_PATH=$(pwd)/../build/lib
+./configure --prefix=$(pwd)/../build \
+			--libdir=$(pwd)/../build/lib \
+			--includedir=$(pwd)/../build/include \
+			--datadir=$(pwd)/../build/docs \
+			--datarootdir=$(pwd)/../build/docs \
+			--disable-shared \
+			--enable-static \
+			--disable-ssp \
+			--disable-asm \
+			--disable-pie \
+			--disable-soname-versions \
+			--enable-minimal
+
+env STENSAL_COMPILER_ENCRYPT=0 make V=1
+make install
