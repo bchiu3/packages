416ab9df4e183aa53cdf0623b7e76cd4392510c0
mlite <mlite@users.noreply.github.com>
2021-05-12 19:05:01 -0700

    add the build script

---
+++ pcre2-10.36/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+mypath=$(dirname $(readlink -f $0))
+export LDFLAGS=-L$(pwd)/../build/lib -static
+export CFLAGS=-O0
+./configure --prefix=$(pwd)/../build \
+			--libdir=$(pwd)/../build/lib \
+			--includedir=$(pwd)/../build/include \
+			--datadir=$(pwd)/../build/docs \
+			--datarootdir=$(pwd)/../build/docs \
+			--disable-shared \
+			--enable-static \
+			--disable-dependency-tracking \
+			--enable-pcre2-16 \
+			--enable-pcre2-32 \
+			--disable-valgrind \
+			--with-pic=non-PIC
+			
+
+
+make -j $(nproc) V=1
+make install
