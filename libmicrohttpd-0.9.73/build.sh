e54c35d0ebfa0ad7786449f7540ec7b60c1deb22
mlite <mlite@users.noreply.github.com>
2021-05-14 13:25:23 -0700

    enable password and basic auth

--- libmicrohttpd-0.9.73/build.sh
+++ libmicrohttpd-0.9.73/build.sh
@@ -7,18 +7,18 @@ LIBS="-static" \
     --includedir=$(pwd)/../build/include \
     --datadir=$(pwd)/../build/docs \
     --datarootdir=$(pwd)/../build/docs \
-	--disable-https \
-	--enable-static \
-	--disable-shared \
-	--disable-gcc-hardening \
-	--disable-sanitizer \
-	--disable-linker-hardening \
-	--disable-nls \
-	--disable-bauth \
-	--disable-dauth \
-	--with-pic=non-PIC \
-	--disable-curl \
-	--enable-heavy-tests \
+    --disable-https \
+    --enable-static \
+    --disable-shared \
+    --disable-gcc-hardening \
+    --disable-sanitizer \
+    --disable-linker-hardening \
+    --disable-nls \
+    --enable-bauth \
+    --enable-dauth \
+    --with-pic=non-PIC \
+    --disable-curl \
+    --enable-heavy-tests \
     --disable-epoll
 
 make -j$(nproc) V=1
