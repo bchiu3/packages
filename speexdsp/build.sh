db5fd36d35c6b0b43e66235e5abb897f682bc29a
mlite <mlite@users.noreply.github.com>
2021-06-07 05:56:17 -0700

    add the build script

---
+++ speexdsp/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+mypath=$(dirname $(readlink -f $0))
+export LDFLAGS=-L$(pwd)/../build/lib -static
+export CFLAGS=-static
+export LT_SYS_LIBRARY_PATH=$(pwd)/../build/lib
+./configure --prefix=$(pwd)/../build \
+			--libdir=$(pwd)/../build/lib \
+			--includedir=$(pwd)/../build/include \
+			--datadir=$(pwd)/../build/docs \
+			--datarootdir=$(pwd)/../build/docs \
+			--disable-shared \
+			--enable-static \
+			--enable-fixed-point
+
+make -j $(nproc) V=1
+make install
