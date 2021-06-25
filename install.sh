07a922fbd74365eec003f44a4f7d1802c48c4d23
mlite <mlite@users.noreply.github.com>
2021-06-24 17:59:15 -0700

    check rsync

--- install.sh
+++ install.sh
@@ -2,9 +2,14 @@
 set -e
 set -o pipefail
 if [ -z ${DEST} ]; then
-  S=$(dirname $(which stensal-c))
-  DEST=${S%%/stensal/bin}
+  S=$(dirname $(which saphc-c))
+  DEST=${S%%/saphc/bin}
 fi
 echo "install to ${DEST}"
 
+which rsync
+if [ $? -ne 0 ]; then
+  echo 'rsync is required, but it is not installed.'
+  exit 1;
+fi
 make DEST=${DEST} install
