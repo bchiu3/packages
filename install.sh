31e2240bb9e47c05ecce48e3c2916c7e2dc54098
mlite <mlite@users.noreply.github.com>
2021-08-13 18:45:45 -0700

    fixes the check of rsync

--- install.sh
+++ install.sh
@@ -7,9 +7,25 @@ if [ -z ${DEST} ]; then
 fi
 echo "install to ${DEST}"
 
-which rsync
-if [ $? -ne 0 ]; then
-  echo 'rsync is required, but it is not installed.'
+error=""
+for i in rsync; do
+  ##echo "$i"
+  command -v $i
+  if [ "$?" -ne 0 ]; then
+    echo "cannot find $i"
+    case $i in
+      *)
+        error="$error $i"
+		;;
+    esac
+  fi
+done
+
+if [ ! "$error" = "" ]; then
+  echo "For Debian and Ubuntu, please run the following command:"
+  echo " sudo apt-get update && sudo apt-get install -y $error"
+  echo "For Other Linux, please refer your distro's installation command"
   exit 1;
 fi
+
 make DEST=${DEST} install
